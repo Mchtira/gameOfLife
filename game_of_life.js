2b1e038aac6e92027cc14cee2363f3d7f563dca1
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-10-03 21:02:13 +0200

    algo and canvas for show it

---
+++ game_of_life.js
@@ -0,0 +1,77 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext('2d')
+
+let game = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+
+const size = Math.sqrt(game.length) //size of array 
+
+const howManyNeighbours = (i) => {
+  let neighbours = 0
+  const tests = [i - size - 1, i - size, i - size + 1, 
+                 i - 1, i + 1,
+                 i + size - 1, i + size, i + size + 1]
+
+  tests.forEach(test => {
+    if (game[test] === 1) neighbours++
+  })
+
+  return neighbours
+}
+
+const drawSquare = (n, index) => {
+  const height = canvas.height
+  const width = canvas.width
+
+  ctx.beginPath()
+  if (n === 0) {
+    ctx.fillStyle = "rgb(250,250,250)"
+    ctx.fillRect(index % size * height / size, Math.floor(index / size) * width / size,
+                 height / size, width / size)
+  } else {
+    ctx.fillStyle = "rgb(0,0,0)"
+    ctx.fillRect(index % size * height / size, Math.floor(index / size) * width / size,
+                 height / size, width / size)
+  }
+  ctx.closePath()
+}
+
+const showCanvas = () => {
+  game = game.map((elem, i) => {
+    const neighbours = howManyNeighbours(i)
+    if (neighbours < 2) {
+      drawSquare(0, i)
+      return 0
+    } else if (neighbours === 2) {
+      drawSquare(elem, i)
+      return elem
+    } else if (neighbours === 3)  {
+      drawSquare(1, i)
+      return 1
+    } else if (neighbours > 3) {
+      drawSquare(0, i) 
+      return 0
+    }
+  })
+  requestAnimationFrame(showCanvas)
+}
+
+requestAnimationFrame(showCanvas)
